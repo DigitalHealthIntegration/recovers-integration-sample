8f0e3d0c2b400458e5022e1a9a27bdb0200e856c
Kashyap Jois <kjois@iprdgroup.com>
2021-05-13 11:16:03 +0530

    Calling home activity directly

--- app/src/main/java/com/iprd/testapplication/MainActivity.java
+++ app/src/main/java/com/iprd/testapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -27,10 +28,10 @@ public class MainActivity extends AppCompatActivity {
     void openApp(Context context) {
         PackageManager manager = context.getPackageManager();
         try {
-            Intent intent = new Intent(Intent.ACTION_MAIN);
+            Intent intent =new Intent("HOME_SCREEN_IPRD"); //new Intent();
             intent.addCategory(Intent.CATEGORY_LAUNCHER);
             manager.resolveActivity(intent, 0);
-            intent.setComponent(new ComponentName("com.iprd.federatedid", "com.iprd.federatedid.SplashScreenActivity"));
+            intent.setComponent(new ComponentName("com.iprd.federatedid", "com.iprd.federatedid.records.ReportActivityNigeriaNet"));
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
         } catch (ActivityNotFoundException e) {
