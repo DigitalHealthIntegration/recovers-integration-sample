0e517508e89b191f1c41eb102c0d788034245548
Kashyap Jois <kjois@iprdgroup.com>
2021-05-13 21:09:02 +0530

    Calling intent with constants

--- app/src/main/java/com/iprd/testapplication/MainActivity.java
+++ app/src/main/java/com/iprd/testapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.iprd.testapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
@@ -10,6 +11,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 
 import com.iprd.federatedid.records.ReportActivityNigeriaNet;
 
@@ -22,19 +25,29 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.btnStart);
         button.setOnClickListener(v -> {
-            openApp(this);
+            openApp(getApplicationContext());
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+//        Toast.makeText(this,data.getExtras().getString(ReportActivityNigeriaNet.BUNDLE_KEY_SHORT_ID),Toast.LENGTH_LONG).show();
     }
 
     void openApp(Context context) {
         PackageManager manager = context.getPackageManager();
         try {
             Intent intent =new Intent(this, ReportActivityNigeriaNet.class); //new Intent();
+            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_USERNAME,"Rohit Kumar");
+            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_DEMOGRAPHIC,"sample-demo");
+//            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_SHORT_ID,"Rohit Kumar");
             intent.addCategory(Intent.CATEGORY_LAUNCHER);
             manager.resolveActivity(intent, 0);
 //            intent.setComponent(new ComponentName("com.iprd.federatedid", "com.iprd.federatedid.records.ReportActivityNigeriaNet"));
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(intent);
+            startActivityForResult(intent,0);
         } catch (ActivityNotFoundException e) {
             e.printStackTrace();
         }
