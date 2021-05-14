097e4648339ea55c2e7d2acdb7a4942b86f9c1fc
Nitin Sabale <nitins@apra.in>
2021-05-14 15:14:05 +0530

    Edit button added

--- app/src/main/java/com/iprd/testapplication/MainActivity.java
+++ app/src/main/java/com/iprd/testapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.iprd.testapplication;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
@@ -18,14 +19,27 @@ import com.iprd.federatedid.records.ReportActivityNigeriaNet;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int NEW_REQUEST_CODE = 401;
+    public static int EDIT_REQUEST_CODE = 402;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.btnStart);
-        button.setOnClickListener(v -> {
-            openApp(getApplicationContext());
+        Button btnNew = findViewById(R.id.btnNew);
+        Button btnEdit = findViewById(R.id.btnEdit);
+        btnNew.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ReportActivityNigeriaNet.class); //new Intent();
+            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_USERNAME, "Rohit Kumar");
+            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_DEMOGRAPHIC, "sample-demo");
+            startActivityForResult(intent, NEW_REQUEST_CODE);
+        });
+
+        btnEdit.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ReportActivityNigeriaNet.class); //new Intent();
+            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_SHORT_ID, "123456789");
+            startActivityForResult(intent, EDIT_REQUEST_CODE);
         });
 
     }
@@ -39,15 +53,15 @@ public class MainActivity extends AppCompatActivity {
     void openApp(Context context) {
         PackageManager manager = context.getPackageManager();
         try {
-            Intent intent =new Intent(this, ReportActivityNigeriaNet.class); //new Intent();
-            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_USERNAME,"Rohit Kumar");
-            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_DEMOGRAPHIC,"sample-demo");
+            Intent intent = new Intent(this, ReportActivityNigeriaNet.class); //new Intent();
+            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_USERNAME, "Rohit Kumar");
+            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_DEMOGRAPHIC, "sample-demo");
 //            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_SHORT_ID,"Rohit Kumar");
             intent.addCategory(Intent.CATEGORY_LAUNCHER);
             manager.resolveActivity(intent, 0);
 //            intent.setComponent(new ComponentName("com.iprd.federatedid", "com.iprd.federatedid.records.ReportActivityNigeriaNet"));
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivityForResult(intent,0);
+            startActivityForResult(intent, 0);
         } catch (ActivityNotFoundException e) {
             e.printStackTrace();
         }
