407dc3ec9abc83cd8ef02555073e03d708838b00
Nitin Sabale <nitins@apra.in>
2021-05-14 20:12:04 +0530

    Open smart health app, build.gradle configurable

--- app/src/main/java/com/iprd/testapplication/MainActivity.java
+++ app/src/main/java/com/iprd/testapplication/MainActivity.java
@@ -1,27 +1,29 @@
 package com.iprd.testapplication;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
-import com.iprd.federatedid.records.ReportActivityNigeriaNet;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String shortId = "";
+    public static int NEW_REQUEST_CODE = 401;
+    public static int EDIT_REQUEST_CODE = 402;
+    public static final String BUNDLE_KEY_USERNAME = "username";
+    public static final String BUNDLE_KEY_SHORT_ID = "shortId";
+    public static final String BUNDLE_KEY_DEMOGRAPHIC = "demographics";
+    public static final String BUNDLE_KEY_NUMBER_OF_FACES = "noFaces";
+    public static final String BUNDLE_KEY_NUMBER_OF_HOUSES = "noHouses";
+    public static final String BUNDLE_KEY_LOCATION_CAPTURED = "locationCaptured";
+
+    private String shortId = "8LDFRK4QVQ7E";
     private String name = "Rohit Kumar";
 
     @Override
@@ -32,35 +34,38 @@ public class MainActivity extends AppCompatActivity {
         Button btnNew = findViewById(R.id.btnNew);
         Button btnEdit = findViewById(R.id.btnEdit);
         btnNew.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ReportActivityNigeriaNet.class); //new Intent();
-            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_USERNAME, name);
-            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_DEMOGRAPHIC, "sample-demo");
-            startActivityForResult(intent, ReportActivityNigeriaNet.NEW_REQUEST_CODE);
-
+            openSmartHealthApp(true);
         });
 
         btnEdit.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ReportActivityNigeriaNet.class); //new Intent();
-            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_SHORT_ID, shortId);
-            intent.putExtra(ReportActivityNigeriaNet.BUNDLE_KEY_USERNAME,name);
-            startActivityForResult(intent, ReportActivityNigeriaNet.EDIT_REQUEST_CODE);
+            openSmartHealthApp(false);
         });
 
     }
+
+    void openSmartHealthApp(boolean isNew) {
+        Intent intent = new Intent("HOME_SCREEN_IPRD");
+        if (isNew) {
+            intent.putExtra(BUNDLE_KEY_USERNAME, name);
+            intent.putExtra(BUNDLE_KEY_DEMOGRAPHIC, "sample-demo");
+        } else {
+            intent.putExtra(BUNDLE_KEY_SHORT_ID, shortId);
+        }
+        intent.setComponent(new ComponentName("com.iprd.federatedid", "com.iprd.federatedid.records.ReportActivityNigeriaNet"));
+        startActivityForResult(intent, NEW_REQUEST_CODE);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(data!=null){
+        if (data != null) {
             Toast.makeText(this,
-                    "Number of faces : "+
-                    data.getExtras().getInt(ReportActivityNigeriaNet.BUNDLE_KEY_NUMBER_OF_FACES) +
-                    "Number of houses : "+
-                    data.getExtras().getInt(ReportActivityNigeriaNet.BUNDLE_KEY_NUMBER_OF_HOUSES),
+                    "Number of faces : " +
+                            data.getExtras().getInt(BUNDLE_KEY_NUMBER_OF_FACES),
                     Toast.LENGTH_LONG).show();
-            shortId = data.getExtras().getString(ReportActivityNigeriaNet.BUNDLE_KEY_SHORT_ID);
+            shortId = data.getExtras().getString(BUNDLE_KEY_SHORT_ID);
         }
 
     }
-
 }
